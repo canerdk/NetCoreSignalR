6eaefa5c5843da8bbcb66b7e100ecbf1b514b1ab
caner <canerdk1995@hotmail.com>
2023-03-16 16:23:07 +0300

    added signalr hub class and configuration

--- SignalRExampleProject/Hubs/MyHub.cs
+++ SignalRExampleProject/Hubs/MyHub.cs
@@ -4,6 +4,16 @@ namespace SignalRExampleProject.Hubs
 {
     public class MyHub : Hub
     {
+        public static List<string> Names { get; set; } = new List<string>();
 
+        public async Task SendNameAsync(string name)
+        {
+            await Clients.All.SendAsync("ReceiveName", name);
+        }
+
+        public async Task GetNamesAsync()
+        {
+            await Clients.All.SendAsync("ReceiveNames", Names);
+        }
     }
 }
